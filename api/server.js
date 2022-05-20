b619d9d33e58b659746c3fc9cdfbad35ffb29dd5
enhopkin@gmail.com <enhopkin@gmail.com>
2022-05-19 20:57:52 -0600

    server, index and package.json up-working on migration

--- api/server.js
+++ api/server.js
@@ -1,12 +1,8 @@
 // build your server here and require it from index.js
-const express = require('express')
+const express = require ('express')
 
-const server= express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-server.use('/', (req, res) => {
-    res.json("LET'S GET READY TO RUMBLE!!")
-})
-
-module.exports= server
+module.exports= server;
