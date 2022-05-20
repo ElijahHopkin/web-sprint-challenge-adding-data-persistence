7f3007e6357519d2a0187aa0a4fe9f3401fc103f
enhopkin@gmail.com <enhopkin@gmail.com>
2022-05-19 20:11:10 -0600

    attempt is stalled--restarting

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+const server= express()
+
+server.use(express.json())
+
+server.use('/', (req, res) => {
+    res.json("LET'S GET READY TO RUMBLE!!")
+})
+
+module.exports= server
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 8000;
+
+server.listen('/', () => {
+    console.log(`listening at port ${PORT}...`)
+} )
